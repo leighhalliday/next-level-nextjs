036b3cc5835bb36784ef60d147511c96e2307da3
Leigh Halliday <leighhalliday@gmail.com>
2021-02-11 13:11:16 -0500

    Fixing tailwind and PPP

--- pages/ppp.tsx
+++ pages/ppp.tsx
@@ -8,7 +8,45 @@ interface IProps {
 
 export default function Ppp({ coupon, discount, countryCode }: IProps) {
   if (!coupon) {
-    return <div>No coupon.</div>;
+    return (
+      <div
+        className="flex flex-1 justify-center items-center bg-gradient-to-r from-teal-500 to-cyan-600"
+        style={{ height: "100vh" }}
+      >
+        <div className="bg-cyan-50 w-full">
+          <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-24 lg:px-8 lg:flex lg:items-center lg:justify-between">
+            <div>
+              <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl">
+                <span className="block">Purchasing Power Parity</span>
+                <span className="block text-cyan-800">
+                  Sorry... I'm not seeing your country.
+                </span>
+              </h2>
+              <span>
+                Not what you expected? Message me on{" "}
+                <a
+                  className="text-cyan-800 font-bold"
+                  href="https://twitter.com/leighchalliday"
+                >
+                  Twitter
+                </a>
+                . I'd love to help!
+              </span>
+            </div>
+            <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+              <div className="inline-flex rounded-md shadow">
+                <a
+                  href={`https://courses.leighhalliday.com/next-level-next-js/buy`}
+                  className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-cyan-800 hover:bg-cyan-900"
+                >
+                  Buy Anyway!
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -35,7 +73,7 @@ export default function Ppp({ coupon, discount, countryCode }: IProps) {
               >
                 Twitter
               </a>
-              .
+              . I'd love to help!
             </span>
           </div>
           <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
@@ -75,12 +113,12 @@ interface IPPPResponse {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const countryCode = (req.headers["cf-ipcountry"] as string | null) ?? "MX";
+  const countryCode = (req.headers["cf-ipcountry"] as string | null) ?? "US";
   let factor: number | undefined = known[countryCode];
 
   if (!factor || typeof factor === "undefined") {
     const response = await fetch(
-      "https://api.purchasing-power-parity.com/?target=ID"
+      `https://api.purchasing-power-parity.com/?target=${countryCode}`
     );
     const data: IPPPResponse = await response.json();
     factor = data?.ppp?.pppConversionFactor ?? 1;
